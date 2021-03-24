b957a5629178eae8110bd19d525d31376a85571a
avi-sharma121 <kumaravinashsharma82@protonmail.com>
2021-03-24 11:01:15 +0530

    init Calculator android app v1.01

---
+++ App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <StatusBar backgroundColor="orange" />
+      <View style={styles.Container}>
+        <Text>Calculator</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  Container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
